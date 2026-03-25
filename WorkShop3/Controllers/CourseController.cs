04c930d124e388fc7500b9913cb55f6a7c88e708
Madancsta <madancsta789@gmail.com>
2026-03-25 07:52:35 +0545

    feat: Added Student and course Controller

---
+++ WorkShop3/Controllers/CourseController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkShop3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController: ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CourseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetAllCourse()
+        {
+            return Ok(_context.Courses.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetACourse(long id)
+        {
+            var course = _context.Courses.Find(id);
+
+            if (course == null)
+            {
+                return NotFound($"Course with id {id} not found");
+            }
+
+            return Ok(course);
+        }
+
+        [HttpPost("")]
+        public IActionResult SaveCourse(Course c)
+        {
+            var check = _context.Courses.Find(c.CourseId);
+            if (check != null)
+            {
+                return ValidationProblem($"Course with id {c.CourseId} already exists.");
+            }
+            _context.Courses.Add(c);
+            _context.SaveChanges();
+            return Ok($"Course {c.Name} saved successfully.");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateCourse(long id, Course c)
+        {
+            var course = _context.Courses.FirstOrDefault(c => c.CourseId == id);
+
+            if (course == null)
+            {
+                return NotFound($"Course with id {id} not found.");
+            }
+            course.Name = c.Name;
+            course.DurationYears = c.DurationYears;
+            _context.SaveChanges();
+            return Ok($"Course {course.Name} updated successfully.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCourse(long id)
+        {
+            var course = _context.Courses.Find(id);
+
+            if (course == null)
+            {
+                return NotFound($"Course with id {id} not found");
+            }
+
+            var name = course.Name;
+
+            _context.Courses.Remove(course);
+            _context.SaveChanges();
+
+            return Ok($"Deleted course {name} with id {id}");
+        }
+
+        [HttpPost("bulk")]
+        public IActionResult SaveCourses(List<Course> s)
+        {
+            _context.Courses.AddRange(s);
+            _context.SaveChanges();
+            return Ok("Saved Courses in bulk");
+        }
+
+        [HttpGet("count")]
+        public IActionResult GetCourseCount()
+        {
+            var count = _context.Courses.Count();
+            return Ok($"Total number Courses are: {count}");
+        }
+    }
+}
--- WorkShop3/Controllers/StudentController.cs
+++ WorkShop3/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Workshop2;
 using WorkShop3;
 
@@ -14,33 +15,147 @@ namespace Workshop2.Controllers
             _context = context;
         }
 
-        [HttpGet("all")]
+        [HttpGet("")]
         public IActionResult GetAllStudent()
         {
             return Ok(_context.Students.ToList());
         }
 
-        [HttpPost("save")]
+        [HttpGet("{id}")]
+        public IActionResult GetAStudent(long id)
+        {
+            var student = _context.Students.Find(id);
+
+            if (student == null)
+            {
+                return NotFound($"Student with id {id} not found");
+            }
+
+            return Ok(student);
+        }
+
+        [HttpPost("")]
         public IActionResult SaveStudent(Student s)
         {
+            var check = _context.Students.Find(s.Id);
+            if (check != null)
+            {
+                return ValidationProblem($"Student with id {s.Id} already exists.");
+            }
             _context.Students.Add(s);
-            return Ok(_context);
+            _context.SaveChanges();
+            return Ok($"Student {s.FirstName} saved successfully.");
         }
 
-        [HttpPatch("update")]
-        public IActionResult UpdateStudent(Student s)
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(long id, Student s)
         {
-            var student = _context.Students.FirstOrDefault(s => s.Id == s.Id);
+            var student = _context.Students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+            {
+                return NotFound($"Student with id {id} not found.");
+            }
             student.FirstName = s.FirstName;
             student.LastName = s.LastName;
-            return Ok(s);
+            student.Email = s.Email;
+            student.DateOfBirth = s.DateOfBirth;
+            student.PhoneNumber = s.PhoneNumber;
+            _context.SaveChanges();
+            return Ok($"Student {student.FirstName} updated successfully.");
         }
 
-        [HttpDelete("delete")]
-        public IActionResult DeleteStudent(int id)
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(long id)
         {
-            _context.Students.Remove(s => s.StudentId == id);
-            return Ok($"Deleted student with id{id}");
+            var student = _context.Students.Find(id);
+
+            if (student == null)
+            {
+                return NotFound($"Student with id {id} not found");
+            }
+
+            var name = student.FirstName;
+
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+
+            return Ok($"Deleted student {name} with id {id}");
+        }
+
+        [HttpGet("{id}/course")]
+        public IActionResult GetStudentCourse(long id)
+        {
+            var course = _context.Enrollments
+                .Where(e => e.StudentId == id)
+                .Join(_context.Courses,
+                      e => e.CourseId,
+                      c => c.CourseId,
+                      (e, c) => new
+                      {
+                          c.CourseId,
+                          c.Name,
+                          c.DurationYears
+                      })
+                .ToList();
+
+            if (course == null || course.Count == 0)
+            {
+                return NotFound($"Student with id {id}, course not found");
+            }
+
+            return Ok(course);
+        }
+
+        [HttpPost("bulk")]
+        public IActionResult SaveStudents(List<Student> s)
+        {
+            _context.Students.AddRange(s);
+            _context.SaveChanges();
+            return Ok("Saved students in bulk");
+        }
+
+        [HttpGet("count")]
+        public IActionResult GetStudentCount()
+        {
+            var count = _context.Students.Count();
+            return Ok($"Total number students are: {count}");
+        }
+
+        [HttpGet("with-courses")]
+        public IActionResult GetStudentsWithCourses()
+        {
+            var data = (from s in _context.Students
+                        join e in _context.Enrollments on s.Id equals e.StudentId
+                        join c in _context.Courses on e.CourseId equals c.CourseId
+                        select new
+                        {
+                            s.Id,
+                            s.FirstName,
+                            CourseId = c.CourseId,
+                            CourseName = c.Name
+                        }).ToList();
+
+            return Ok(data);
+        }
+
+        [HttpGet("full-details")]
+        public IActionResult GetFullDetails()
+        {
+            var data = ( from s in _context.Students
+                         join e in _context.Enrollments on s.Id equals e.StudentId
+                         join c in _context.Courses on e.CourseId equals c.CourseId
+                         join m in _context.Modules on c.CourseId equals m.CourseId
+                         select new
+                         {
+                             s.Id,
+                             s.FirstName,
+                             CourseId = c.CourseId,
+                             CourseName = c.Name,
+                             ModuleId = m.ModuleId,
+                             ModuleTitle = m.Title,
+                         }).ToList();
+            return Ok(data);
         }
     }
 }
